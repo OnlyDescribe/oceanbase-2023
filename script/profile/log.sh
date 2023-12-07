1dcf2df0f6a4e870324434c606b1e81a2da0ecf3
oldcb <only_describe@outlook.com>
2023-12-08 01:16:44 +0800

    perf: do_restart

--- script/profile/log.sh
+++ script/profile/log.sh
@@ -11,15 +11,16 @@ grep --exclude='analyze.log' --exclude='bootstrap.log' -E 'execute_bootstrap sta
 grep --exclude='analyze.log' --exclude='bootstrap.log' -E 'create_all_schema execute success' * >> analyze.log
 grep --exclude='analyze.log' --exclude='bootstrap.log' -E 'refresh_schema execute success' * >> analyze.log
 grep --exclude='analyze.log' --exclude='bootstrap.log' -E 'wait_all_rs_in_service execute success' * >> analyze.log
+grep --exclude='analyze.log' --exclude='bootstrap.log' -E 'start do_restart' * >> analyze.log
 grep --exclude='analyze.log' --exclude='bootstrap.log' -E 'finish do_restart' * >> analyze.log
 
 # for 优化 6
 # grep --exclude='analyze.log' --exclude='bootstrap.log' -E 'start ob_service end' * >> analyze.log
-grep --exclude='analyze.log' --exclude='bootstrap.log' -E 'start observer begin' * >> analyze.log
-grep --exclude='analyze.log' --exclude='bootstrap.log' -E 'success to refresh server configure' * >> analyze.log
-grep --exclude='analyze.log' --exclude='bootstrap.log' -E 'check if multi tenant synced' * >> analyze.log
-grep --exclude='analyze.log' --exclude='bootstrap.log' -E 'check if timezone usable' * >> analyze.log
-grep --exclude='analyze.log' --exclude='bootstrap.log' -E 'observer start service' * >> analyze.log
+# grep --exclude='analyze.log' --exclude='bootstrap.log' -E 'start observer begin' * >> analyze.log
+# grep --exclude='analyze.log' --exclude='bootstrap.log' -E 'success to refresh server configure' * >> analyze.log
+# grep --exclude='analyze.log' --exclude='bootstrap.log' -E 'check if multi tenant synced' * >> analyze.log
+# grep --exclude='analyze.log' --exclude='bootstrap.log' -E 'check if timezone usable' * >> analyze.log
+# grep --exclude='analyze.log' --exclude='bootstrap.log' -E 'observer start service' * >> analyze.log
 
 # 耗时久的过程
 # grep --exclude='analyze.log' --exclude='bootstrap.log' -E 'cost=(1[0-9]{5,}|[2-9][0-9]{5,})' * >> analyze.log
